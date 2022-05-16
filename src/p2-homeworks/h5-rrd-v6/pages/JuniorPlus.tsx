ea1c26da61c2b5520ec96c0b002e55026fe677ec
and-gurin <and.gurin@gmail.com>
2022-05-16 15:31:37 +0300

    fix: complete home-work number 9

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
+import HW9 from "../../h9/HW9";
+import HW8 from "../../h8/HW8";
 
 
 function JuniorPlus() {
     return (
         <div>
             <HW7/>
+            <HW8/>
+            <HW9/>
 
         </div>
     )
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {brotliCompress} from "zlib";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,25 +8,25 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+       clearInterval(timerId)
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = +setInterval(() => {
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/> // fix with date
+    const stringDate = date?.toLocaleDateString() || <br/>
 
     return (
         <div>
@@ -36,11 +37,11 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
+            {show ? (
                 <div>
                     {stringDate}
                 </div>
-            )}
+            ) : (<br/>)}
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
