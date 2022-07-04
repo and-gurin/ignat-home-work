5ff89226ab0982b47d58b2ddbf61cdcb4cea607f
and-gurin <and.gurin@gmail.com>
2022-07-04 11:25:15 +0300

    feat: add home-work number 10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import s from './HW10.module.css'
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading)
+    const dispatch = useDispatch()
+    //const loading = false
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(()=>{
+            dispatch(loadingAC(false))}, 1000)
         console.log('loading...')
-    };
+     };
 
     return (
         <div>
@@ -18,11 +24,13 @@ function HW10() {
 
             {/*should work (должно работать)*/}
             {loading
-                ? (
-                    <div>крутилка...</div>
+                ? (<div className={`${s.loader} ${s.loaderTwo}`}></div>
+
                 ) : (
                     <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
+                        <SuperButton
+                            onClick={setLoading}
+                        >set loading...</SuperButton>
                     </div>
                 )
             }
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,18 @@
-const initState = {
-
+const initialState = {
+    loading: false
 }
+export type InitialStateType = typeof initialState;
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initialState, action: LoadingActionType): InitialStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'SWITCH-LOADING': {
+            return {
+                ...state, loading: action.value
+            }
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (value: boolean) => ({type: 'SWITCH-LOADING', value: value} as const) // fix any
+export type LoadingActionType = ReturnType<typeof loadingAC>
